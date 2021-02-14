959d00dcde24bc289c33bd6a199c7c25ea49736a
haoweili1096 <haoweili007@gmail.com>
2021-02-13 20:19:56 -0800

    install React, webpack, babel and many other libraries. Use webpack to compile JS to make it able to use import. closes #4.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+// main process
 const {app, BrowserWindow} = require('electron');
 
 function createWindow(){
+    // browser window run by renderer process
     const win = new BrowserWindow({
         width: 800,
-        height: 800,
+        height: 600,
         backgroundColor: "white", 
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false
         }
     })
 
     win.loadFile('index.html');
+
 }
 
 app.whenReady().then(createWindow);
