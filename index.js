ab3874cc30cf5d65216b11eeb69f9dc4d3bddad1
haoweili1096 <haoweili007@gmail.com>
2021-02-13 21:02:13 -0800

    electron reload under dev environment. closes #6.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // main process
 const {app, BrowserWindow} = require('electron');
 
+//only use electron-reload in development environment
+const path = require('path');
+const isDev = !app.isPackaged;
+
 function createWindow(){
     // browser window run by renderer process
     const win = new BrowserWindow({
@@ -13,7 +17,13 @@ function createWindow(){
     })
 
     win.loadFile('index.html');
+    isDev && win.webContents.openDevTools();
+}
 
+if (isDev) {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+    })
 }
 
 app.whenReady().then(createWindow);
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<h1>I am a React app!</h1>, document.getElementById('electronChat'));
+ReactDOM.render(<App />, document.getElementById('electronChat'));
