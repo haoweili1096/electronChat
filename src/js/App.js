7ddafc4a41ad39fa6026a322236d3907b1c948f0
haoweili1096 <haoweili007@gmail.com>
2021-02-14 17:18:57 -0800

    Adding routers to this app. closes #12.

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import HomeView from './views/Home';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
-export default function App(){
+export default function App() {
     return (
-        <HomeView />
+        <Router>
+            <Navbar />
+            <div className='content-wrapper'>
+                <Switch>
+                    <Route path="/settings">
+                        <h1>I am Settings View</h1>
+                    </Route>
+                    <Route path="/login">
+                        <h1>I am Login View</h1>
+                    </Route>
+                    <Route path="/register">
+                        <h1>I am Register View</h1>
+                    </Route>
+                    <Route path="/">
+                        <HomeView />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
