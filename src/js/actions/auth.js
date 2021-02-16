e3c3874b51c01b5b9d9e59393cc006bc32a140d5
haoweili1096 <haoweili007@gmail.com>
2021-02-15 18:36:51 -0800

    Add loading view and spinner. closes #30.

--- src/js/actions/auth.js
+++ src/js/actions/auth.js
@@ -1,7 +1,8 @@
 import * as api from '../api/auth';
 
 export const registerUser = formData => dispatch => {
-    api.register(formData)
+    dispatch({type: 'AUTH_REGISTER_INIT'})
+    return api.register(formData)
         .then(user => {
             dispatch({
                 type: 'AUTH_REGISTER_SUCCESS'
@@ -10,21 +11,22 @@ export const registerUser = formData => dispatch => {
         })
 }
 
-export const logout = () => dispatch => {
-    api
-        .logout()
-        .then(_ => dispatch({type: 'AUTH_LOGOUT_SUCCESS'}))
-}
-
 export const loginUser = formData => dispatch => {
-    api
+    dispatch({type: 'AUTH_LOGIN_INIT'})
+    return api
         .login(formData)
         .then(_ => dispatch({type: 'AUTH_LOGIN_SUCCESS'}))
 }
 
+export const logout = () => dispatch => {
+    return api
+        .logout()
+        .then(_ => dispatch({type: 'AUTH_LOGOUT_SUCCESS'}))
+}
+
 export const listenToAuthChanges = () => dispatch => {
     dispatch({type: 'AUTH_ON_INIT'});
-    api.onAuthStateChanges(authUser => {
+    return api.onAuthStateChanges(authUser => {
         if(authUser){
             dispatch({type: 'AUTH_ON_SUCCESS', user: authUser});
         }
