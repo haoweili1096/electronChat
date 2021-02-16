29b0d3ac57c26a93bc83d6cca758e51ba5711221
haoweili1096 <haoweili007@gmail.com>
2021-02-15 22:03:07 -0800

    layout HOC. closes #39.

--- src/js/views/Chat.js
+++ src/js/views/Chat.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import ChatUsersList from '../components/ChatUsersList'; 
+import ChatUsersList from '../components/ChatUsersList';
 import ViewTitle from '../components/shared/ViewTitle';
 import ChatMessagesList from '../components/ChatMessagesList';
-import BaseLayout from '../layouts/Base';
+import { withBaseLayout } from '../layouts/Base';
 
-export default function Chat() {
+function Chat() {
     const { id } = useParams();
     return (
-        <BaseLayout canGoBack>
-            <div className="row no-gutters fh">
-                <div className="col-3 fh">
-                    <ChatUsersList />
-                </div>
-                <div className="col-9 fh">
-                    <ViewTitle text={`Joined channel: ${id}`}/>
-                    <ChatMessagesList />
-                </div>
+        <div className="row no-gutters fh">
+            <div className="col-3 fh">
+                <ChatUsersList />
             </div>
-        </BaseLayout>
+            <div className="col-9 fh">
+                <ViewTitle text={`Joined channel: ${id}`} />
+                <ChatMessagesList />
+            </div>
+        </div>
     )
 }
+
+export default withBaseLayout(Chat, {canGoBack: true});
