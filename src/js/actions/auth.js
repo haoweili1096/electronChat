e9841e84082273888e20d1247b65cb9cf87d1073
haoweili1096 <haoweili007@gmail.com>
2021-02-15 18:13:45 -0800

    logout. closes #28.

--- src/js/actions/auth.js
+++ src/js/actions/auth.js
@@ -10,6 +10,12 @@ export const registerUser = formData => dispatch => {
         })
 }
 
+export const logout = () => dispatch => {
+    api
+        .logout()
+        .then(_ => dispatch({type: 'AUTH_LOGOUT_SUCCESS'}))
+}
+
 export const listenToAuthChanges = () => dispatch => {
     dispatch({type: 'AUTH_ON_INIT'});
     api.onAuthStateChanges(authUser => {
